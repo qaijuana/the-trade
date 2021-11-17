45e55bf9890b37c2198791eec370741c08ed4836
qaijuana <qai.ableman@gmail.com>
2021-11-17 16:25:18 +0800

    login and jwt completed however, unlinked to postgresql

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,20 +1,48 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
-const router = require("router");
+const router = express.Router();
+
+const users = [];
+console.log(users);
+
+//! CREATE USER
+router.post("/new", async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(7);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const user = {
+            username: req.body.username,
+            password: hashedPassword
+        };
+        users.push(user);
+        res.sendStatus(201);
+        console.log(users)
+    } catch (error) {
+        res.sendStatus(500);
+    }
 
 
-// create
 
-router.post("/new", (req, res) => {
-    console.log(req)
-    res.json("lol")
+
 })
 
-// show all
+//! LOGIN
 
-router.get("/show", (req, res) => {
-    console.log(req, res)
-    res.json("lol");
+router.post("/login", async (req, res) => {
+    //! SWITCH TO DATABASE QUERY HERE 
+    const user = users.find(user => user.name === req.body.name)
+    if (!user) {
+        return res.status(400).send("Invalid Username/Password");
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send("Success");
+        } else {
+            res.send("You did an oopsie");
+        }
+    } catch (error) {
+        res.sendStatus(400)
+    }
 })
 
 // show one
@@ -39,3 +67,5 @@ router.delete("/:id", (req, res) => {
     const { id } = req.params;
     res.json("lol");
 })
+
+module.exports = router;
