067e45893ece7921334c97c9daa9bc77227b1801
qaijuana <qai.ableman@gmail.com>
2021-11-22 03:12:41 +0800

    adding routes and cloudinary for image upload

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,26 @@ import "./styles/App.css"
 
 function App() {
 
-  const [logged, setLogged] = useState(true);
+  const [logged, setLogged] = useState(false);
+  const [uploadInfo, setUploadInfo] = useState();
+  const [status, setStatus] = useState("pending");
 
   function handleLogin(e) {
     e.preventDefault();
+    console.log("login", !logged)
     setLogged(!logged)
   }
   function handleLogout(e) {
     e.preventDefault();
-    setLogged(true)
+    console.log("logout", logged)
+    setLogged(!logged)
   }
 
   function UserPage() {
     return (
-      <Link to="/user/edit">Edit</Link>
+      <Link to="/user/edit">
+        <h1>edit</h1>
+      </Link>
     )
   }
 
@@ -33,28 +39,88 @@ function App() {
     )
   }
 
-  function NavBar() {
+  function CreateList() {
 
+    function handleUpload(event) {
+      event.preventDefault();
+      const formData = new FormData();
+      formData.append("file", uploadInfo)
+      formData.append("upload_preset", "string")
+
+      const sendCloudinary = async () => {
+        setStatus("Loading");
+        const res = await fetch(
+          `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`,
+          formData
+        )
+        const data = res.json();
+        console.log(data)
+        
+      }
+
+
+
+      // const sendData = async () => {
+      //   setStatus("Loading...")
+      //   const res = await fetch("/api/users/new", {
+      //     method: "POST",
+      //     body: JSON.stringify(signUp),
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     },
+      //   })
+      //   const data = await res.json();
+      //   console.log(data)
+      // }
+      // sendData();
+
+
+    }
 
     return (
-      <div className="header">
-        <div className="nav-1">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/marketplace"}>Shop</Link>
-        </div>
-        {(logged) ?
-          <div className="nav-2">
+      <>
+        <h1>Create</h1>
+        <img src="" alt="" />
+        <input type="file" onChange={(event) => { setUploadInfo(event.target.files[0]) }} />
+        <button onClick={handleUpload}>Upload</button>
+        {uploadInfo ? <h6>{uploadInfo}MB</h6> : ""}
+      </>
+    )
+  }
+
+  function NavBar() {
+
+    function AuthUser() {
+
+      return (
+        (logged) ?
+          <div className="header-2">
             <Link to={"/learn"}>Learn</Link>
             <Link to={"/login"} onClick={handleLogin}>Login</Link>
             <Link to={"/signup"}>Sign Up</Link>
           </div>
           :
-          <div className="nav-2">
+          <div className="header-2">
+
+            <Link to={"/create"}>Add</Link>
             <Link to={"/learn"}>Learn</Link>
-            <Link to={"/list"}>Add</Link>
-            <Link to={"/user"}>Profile</Link>
-            <Link to={"/logout"} onclick={handleLogout}>Logout</Link>
-          </div>}
+
+            <div className="header-profile">
+              <Link to={"/user"}>Profile</Link>
+              <Link to={"/"} onClick={handleLogout}>Logout</Link>
+            </div>
+          </div>
+      )
+    }
+
+    return (
+      <div className="header">
+        <div className="header-1">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/marketplace"}>Shop</Link>
+        </div>
+        <AuthUser />
+
       </div>
 
     )
@@ -74,6 +140,7 @@ function App() {
             </Route>
             <Route path="/user/edit" element={<Page title={"edit"} />} />
             <Route path="/learn" element={<LearnReact />} />
+            <Route path="/create" element={<CreateList />} />
 
 
           </Routes>
